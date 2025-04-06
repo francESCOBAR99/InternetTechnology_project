53d81b3f305df7db0c313e6cca5bf0a34754ab92
livami <livamichelle@gmail.com>
2025-04-06 11:53:53 +0200

    Update Item.java
    updated Item.java created through copilot

--- FHNW Thrifting/src/main/java/domain/Item.java
+++ FHNW Thrifting/src/main/java/domain/Item.java
@@ -1,1 +1,58 @@
+package com.platform.data.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Item {
+
+    @Id
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+
+    @ManyToOne
+    private Category category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
