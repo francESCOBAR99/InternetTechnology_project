3cfc15755a4a430c5e21d8f243da382b7d97bd94
livami <livamichelle@gmail.com>
2025-04-07 14:40:40 +0200

    Add files via upload

---
+++ thrift/src/main/java/ch/fhnw/thrift/data/domain/User.java
@@ -0,0 +1,67 @@
+package ch.fhnw.thrift.data.domain;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Item {
+
+    @Id
+    @JsonIgnore
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+
+    @ManyToOne(mappedBy = "item")
+    private List<Category> categoryList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name; 
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public List<Category> getCategoryList() {
+        return categoryList;
+    }
+
+    public void setCategoryList(List<Category> categoryList) {
+        this.categoryList = categoryList;
+    }
+
+    
+    
+}
