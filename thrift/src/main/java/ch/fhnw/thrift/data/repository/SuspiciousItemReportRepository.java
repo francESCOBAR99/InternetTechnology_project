ab87f89d0b260088e0cb20ae40d35e347e754892
livami <livamichelle@gmail.com>
2025-05-22 17:52:21 +0200

    Update SuspiciousItemReportRepository.java

--- thrift/src/main/java/ch/fhnw/thrift/data/repository/SuspiciousItemReportRepository.java
+++ thrift/src/main/java/ch/fhnw/thrift/data/repository/SuspiciousItemReportRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import ch.fhnw.pizza.data.domain.SuspiciousItemReport;
+import ch.fhnw.thrift.data.domain.SuspiciousItemReport;
 
 public interface SuspiciousItemReportRepository extends JpaRepository<SuspiciousItemReport, Long> {
     List<SuspiciousItemReport> findByItemId(Long itemId);
