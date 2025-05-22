9cf14cd9210052ffc4adf3c4bd4a70ec852c27ba
livami <livamichelle@gmail.com>
2025-05-22 17:52:10 +0200

    Update ItemRepository.java

--- thrift/src/main/java/ch/fhnw/thrift/data/repository/ItemRepository.java
+++ thrift/src/main/java/ch/fhnw/thrift/data/repository/ItemRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import ch.fhnw.pizza.data.domain.Item;
+import ch.fhnw.thrift.data.domain.Item;
 
 @Repository
 //JpaRepository should be typed to the domain class and an ID type
