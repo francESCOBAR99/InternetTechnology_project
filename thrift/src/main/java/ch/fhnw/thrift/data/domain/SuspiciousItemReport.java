2cf72a20bd0bd2a3ad277ce34c4e1f72392b5071
livami <livamichelle@gmail.com>
2025-05-22 17:42:02 +0200

    Update and rename User.java to SuspiciousItemReport.java

---
+++ thrift/src/main/java/ch/fhnw/thrift/data/domain/SuspiciousItemReport.java
@@ -0,0 +1,68 @@
+package ch.fhnw.thrift.data.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class SuspiciousItemReport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Item item;
+
+    private String reportedBy; // e.g., username or email
+
+    private String reason;
+
+    private LocalDateTime reportedAt = LocalDateTime.now();
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public String getReportedBy() {
+        return reportedBy;
+    }
+
+    public void setReportedBy(String reportedBy) {
+        this.reportedBy = reportedBy;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public LocalDateTime getReportedAt() {
+        return reportedAt;
+    }
+
+    public void setReportedAt(LocalDateTime reportedAt) {
+        this.reportedAt = reportedAt;
+    }
+}
