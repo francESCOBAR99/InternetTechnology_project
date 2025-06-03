988fe74584af7af1f6897641db4040f2f5213883
livami <livamichelle@gmail.com>
2025-06-03 20:03:22 +0200

    Revert "Added user management functionality:"
    This reverts commit 0a29c5cdd262edafba3bcdbc2eddf2290405cc05.

--- thrift/src/main/java/ch/fhnw/thrift/data/repository/UserRepository.java
+++ thrift/src/main/java/ch/fhnw/thrift/data/repository/UserRepository.java
@@ -1,13 +1,1 @@
-package ch.fhnw.thrift.data.repository;
 
-import ch.fhnw.thrift.data.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    // Find a user by username (for authentication)
-
-    Optional<User> findByUsername(String username);
-
-}
