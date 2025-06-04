be691ba9b0fcf9fe7ada60ce2fcde39e458713c9
livami <livamichelle@gmail.com>
2025-06-04 16:58:35 +0200

    Update UserRepository.java

--- thrift/src/main/java/ch/fhnw/thrift/data/repository/UserRepository.java
+++ thrift/src/main/java/ch/fhnw/thrift/data/repository/UserRepository.java
@@ -1,1 +1,16 @@
+package ch.fhnw.thrift.data.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ch.fhnw.thrift.data.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findByUsernameAndPassword(String username, String password);
+    List<User> findAllByRole(String role);
+}
 
