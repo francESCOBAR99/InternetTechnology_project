5cbb65347e49b37de5c5f2c1fd93d33ea26a4a9b
livami <livamichelle@gmail.com>
2025-06-09 14:58:26 +0000

    automatically generates id for new users

--- thrift/src/main/java/ch/fhnw/thrift/data/domain/User.java
+++ thrift/src/main/java/ch/fhnw/thrift/data/domain/User.java
@@ -10,14 +10,13 @@ import jakarta.persistence.Table;
 
 @Entity 
 @Table(name = "users")
-
 public class User {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Hidden
     @Column(name = "id", nullable = false)
-    private Long userId;
+    private Long id;
 
     @Column(name = "username", unique = true, nullable = false)
     private String username;
@@ -29,11 +28,11 @@ public class User {
     private String role;
 
     public Long getId() {
-        return userId;
+        return id;
     }
 
     public void setId(Long id) {
-        this.userId = id;
+        this.id = id;
     }
 
     public String getUsername() {
