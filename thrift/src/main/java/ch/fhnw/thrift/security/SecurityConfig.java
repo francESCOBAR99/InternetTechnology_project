14ba1ef6ff5a744ae12c852e8de6fc8a743cd034
livami <livamichelle@gmail.com>
2025-06-13 23:27:16 +0200

    Update SecurityConfig.java

--- thrift/src/main/java/ch/fhnw/thrift/security/SecurityConfig.java
+++ thrift/src/main/java/ch/fhnw/thrift/security/SecurityConfig.java
@@ -20,6 +20,47 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+ /*   @Bean
+    public UserDetailsService users() {
+        //Create two users with different roles and add them to the in-memory user store
+
+        return new InMemoryUserDetailsManager(
+            User.withUsername("myuser")
+                .password("{noop}password") //create user with an encrypted password instead of the plain text password
+              //  .password("password")
+                .authorities("READ","ROLE_USER")
+                .build(), 
+            User.withUsername("myadmin")
+                .password("{noop}admin") //create user with an encrypted password instead of the plain text password
+               // .password("admin")
+                .authorities("READ","ROLE_ADMIN")
+                .build());
+
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests( auth -> auth
+                        .requestMatchers("/users").hasRole("USER") //note that the role need not be prefixed with "ROLE_"
+                        .requestMatchers("/users/admin").hasRole("ADMIN") //note that the role need not be prefixed with "ROLE_"
+                        .requestMatchers(
+                                                    "/", //allow access to the home page
+                                                    "/swagger-ui.html", //allow access to the swagger UI
+                                                    "/v3/api-docs/**",  //allow access to the swagger API documentation
+                                                    "/swagger-ui/**",   //allow access to the swagger UI
+                                                    "/h2-console/**")   //allow access to the h2-console
+                                                    .permitAll() 
+                        .anyRequest().authenticated()
+                      //  .anyRequest().hasAuthority("SCOPE_READ")       
+                )       
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable())) //needed to allow access to the h2-console
+                .formLogin(withDefaults()) //need to include a static import for withDefaults, see the imports at the top of the file
+                .httpBasic(withDefaults())
+                .build(); 
+    } */
+
     @Bean
     public UserDetailsService users() {
         //Create two users with different roles and add them to the in-memory user store
@@ -43,8 +84,8 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests( auth -> auth
-                        .requestMatchers("/offer").hasRole("USER") //note that the role need not be prefixed with "ROLE_"
-                        .requestMatchers("/offer/items/**").hasRole("ADMIN") //note that the role need not be prefixed with "ROLE_"
+                        .requestMatchers("/offer/user").hasRole("USER") //note that the role need not be prefixed with "ROLE_"
+                        .requestMatchers("/offer/admin").hasRole("ADMIN") //note that the role need not be prefixed with "ROLE_"
                         .requestMatchers("/offer/**",
                                                     "/**", //allow access to the home page
                                                     "/swagger-ui.html", //allow access to the swagger UI
@@ -62,5 +103,7 @@ public class SecurityConfig {
 
 
 
+        
+}
         
 }
